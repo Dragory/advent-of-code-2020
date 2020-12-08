97daa0f13f9f1d67f86c006bb679c5127d77e2a1
Dragory <2606411+Dragory@users.noreply.github.com>
2020-12-09 01:02:49 +0200

    day7 part 2 + small refactoring

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,49 +4,68 @@ use std::fs;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-// "How many bag colors can eventually contain at least one shiny gold bag?"
-fn part1(rules: &Vec<&str>) -> u32 {
+struct ContainedBag {
+    bag: String,
+    count: u8
+}
+
+struct Rule {
+    bag: String,
+    contained_bags: Vec<ContainedBag>
+}
+
+// parse a rule of the form "shiny gold bags contain 2 dark red bags."
+fn parse_rule(rule: &str) -> Rule {
     lazy_static! {
         static ref RULE_RE: Regex = Regex::new(r"^(.+?) bags contain (.+?)$").unwrap();
         static ref RULE_CONTAIN_RE: Regex = Regex::new(r"(\d+) (.+?) bags?").unwrap();
     }
 
-    // create a map of each bag color that has parents (is contained within another bag)
+    let rule_parts = RULE_RE.captures(rule);
+    if rule_parts.is_none() {
+        panic!("Invalid rule: {}", rule);
+    }
+
+    let rule_parts_uw = rule_parts.unwrap();
+
+    let parent_bag = rule_parts_uw[1].to_string();
+
+    let mut contained_bags: Vec<ContainedBag> = vec![];
+    let contained_bags_raw = &rule_parts_uw[2];
+    let contained_bags_iter = RULE_CONTAIN_RE.captures_iter(contained_bags_raw);
+    for contained_bag in contained_bags_iter {
+        contained_bags.push(ContainedBag {
+            bag: contained_bag[2].to_string(),
+            count: contained_bag[1].parse::<u8>().unwrap()
+        });
+    }
+
+    return Rule {
+        bag: parent_bag,
+        contained_bags
+    };
+}
+
+// "How many bag colors can eventually contain at least one shiny gold bag?"
+fn part1(rules: &Vec<&str>) -> u32 {
+    // create a map of each bag color and their parents (bags they can be contained in)
     let mut bag_parents: HashMap<String, Vec<String>> = HashMap::new();
 
     for rule in rules {
-        let rule_parts = RULE_RE.captures(rule);
-        if rule_parts.is_none() {
-            panic!("Invalid rule: {}", rule);
-        }
-
-        let rule_parts_uw = rule_parts.unwrap();
-        let parent_bag = rule_parts_uw[1].to_string();
-        let contained_bags_raw = &rule_parts_uw[2];
-
-        let contained_bags_iter = RULE_CONTAIN_RE.captures_iter(contained_bags_raw);
-        for contained_bag in contained_bags_iter {
-            let contained_bag_name = contained_bag[2].to_string();
-            match bag_parents.get_mut(&contained_bag_name) {
-                Some(x) => x.push(parent_bag.clone()),
+        let parsed_rule = parse_rule(rule);
+        for contained_bag in parsed_rule.contained_bags {
+            match bag_parents.get_mut(&contained_bag.bag) {
+                Some(x) => x.push(parsed_rule.bag.clone()),
                 None => {
-                    bag_parents.insert(contained_bag_name, vec![parent_bag.clone()]);
+                    bag_parents.insert(contained_bag.bag, vec![parsed_rule.bag.clone()]);
                     ()
                 }
             }
         }
     }
 
-    let foo = bag_parents.get("shiny gold").unwrap();
-    println!("shiny gold direct parents: {}", foo.join(", "));
-
+    // from that map, find how many parents the shiny gold bag has
     let parents = find_deep_parents(&bag_parents, "shiny gold", None);
-    let parent_names = parents.iter()
-        .map(|x| String::from(x.clone()))
-        .collect::<Vec<_>>()
-        .join(", ");
-
-    println!("parents: {}", parent_names);
     return parents.len() as u32;
 }
 
@@ -74,6 +93,36 @@ fn find_deep_parents<'a>(bag_parents: &'a HashMap<String, Vec<String>>, ref_bag:
     return real_found_parents;
 }
 
+// "How many individual bags are required inside your single shiny gold bag?"
+fn part2(rules: &Vec<&str>) -> u32 {
+    // build map of bag -> contained bags
+    let mut bag_map: HashMap<String, Vec<ContainedBag>> = HashMap::new();
+
+    for rule in rules {
+        let parsed_rule = parse_rule(rule);
+        bag_map.insert(parsed_rule.bag, parsed_rule.contained_bags);
+    }
+
+    // count how many bags are nested within a shiny gold bag
+    return count_contained_bags(&bag_map, "shiny gold");
+}
+
+// recursively count how many contained/inner bags a specific bag has in the bag map
+fn count_contained_bags(bag_map: &HashMap<String, Vec<ContainedBag>>, ref_bag: &str) -> u32 {
+    let contained_bags = bag_map.get(ref_bag);
+    if contained_bags.is_none() {
+        return 0;
+    }
+
+    let mut count: u32 = 0;
+    for contained_bag in contained_bags.unwrap() {
+        count += contained_bag.count as u32;
+        count += count_contained_bags(bag_map, &contained_bag.bag) * contained_bag.count as u32;
+    }
+
+    return count;
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt")
         .expect("Error reading input file");
@@ -83,4 +132,8 @@ fn main() {
     println!("Part 1:");
     let part1_result = part1(&rules);
     println!("Result: {}", part1_result);
+
+    println!("Part 2:");
+    let part2_result = part2(&rules);
+    println!("Result: {}", part2_result);
 }
